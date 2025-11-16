72f206eeba79f8569af8a985f3a4c33778847cff
Ivan Sergeev <ivan.n.sergeev@gmail.com>
2025-11-16 17:28:11 +0500

    Init src

---
+++ xray-monitor.cpp
@@ -0,0 +1,12 @@
+﻿// xray-monitor.cpp: определяет точку входа для приложения.
+//
+
+#include "xray-monitor.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
